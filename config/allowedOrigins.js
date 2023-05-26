bc1d5945fee215e3b747551d3e0a514ce840265e
Jhosaim <86936147+jhosR@users.noreply.github.com>
2023-05-26 18:42:13 -0500

    hotfix cors 2

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,7 @@
 const AllowedOrigins = [
     'http://localhost:5173',
-    'https://iautos.pe/',
-    'http://www.obenprime-dev.com/'
+    'https://iautos.pe',
+    'http://www.obenprime-dev.com'
 ];
 
 module.exports = AllowedOrigins;
