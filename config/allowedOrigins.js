a3f9cc10929e065828f633e823e377dd3c0e4651
Jhosaim <86936147+jhosR@users.noreply.github.com>
2023-05-31 16:01:44 -0500

    cors fix

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,7 +2,7 @@ const AllowedOrigins = [
     'http://localhost:5173',
     'https://iautos.pe',
     'http://www.obenprime-dev.com',
-    'http://35.169.215.24/'
+    'http://35.169.215.24'
 ];
 
 module.exports = AllowedOrigins;
